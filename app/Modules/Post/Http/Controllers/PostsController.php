4633be0fdf4650a3c9a71223506cc91c6039d92e
Rannie Ollit <rannie@mandnweb.com>
2021-12-07 21:48:33 +0800

    WIP

--- app/Modules/Post/Http/Controllers/PostsController.php
+++ app/Modules/Post/Http/Controllers/PostsController.php
@@ -3,6 +3,7 @@
 namespace App\Modules\Post\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Modules\Post\Models\Post;
 use Illuminate\Http\Request;
 
 class PostsController extends Controller
@@ -14,7 +15,7 @@ class PostsController extends Controller
      */
     public function index()
     {
-        //
+        return Post::paginate();
     }
 
     /**
@@ -25,40 +26,40 @@ class PostsController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        return Post::create($request->all());
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  int  $id
+     * @param  Post $post
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show(Post $post)
     {
-        //
+        return $post;
     }
 
     /**
      * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
+     * @param  Post $post
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(Request $request, Post $post)
     {
-        //
+        return tap($post)->update($request->all());
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  int  $id
+     * @param  Post $post
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(Post $post)
     {
-        //
+        return $post->delete();
     }
 }
---
+++ app/Modules/Post/Models/Post.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Modules\Post\Models;
+
+use Database\Factories\PostFactory;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    use HasFactory;
+
+    protected $guarded = ['id'];
+
+    /**
+     * Create a new factory instance for the model.
+     *
+     * @return \Illuminate\Database\Eloquent\Factories\Factory
+     */
+    protected static function newFactory()
+    {
+        return PostFactory::new();
+    }
+}
