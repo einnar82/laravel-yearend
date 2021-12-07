7c373e5592c055ef5bebc05a0de720b267d6f45d
Rannie Ollit <rannie@mandnweb.com>
2021-12-07 22:12:22 +0800

    WIP

---
+++ app/Modules/Post/Manager/PostExportManager.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Modules\Post\Manager;
+
+use Illuminate\Support\Manager;
+
+class PostExportManager extends Manager
+{
+    /**
+     * Get the default driver name.
+     *
+     * @return string
+     */
+    public function getDefaultDriver()
+    {
+
+    }
+
+    /**
+     * Export posts data in JSON format
+     */
+    public function createJsonDriver()
+    {
+
+    }
+
+    /**
+     * Export posts data in CSV format
+     */
+    public function createCsvDriver()
+    {
+
+    }
+}
